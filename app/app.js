f9d814cdf1c3835f072e4d84e8593fb9d10fc5a1
Coda Phillips <codaphillips@dyn-10-136-34-99.local.uwnet.wisc.edu>
2015-04-18 13:31:55 -0500

    Change myApp name to wikiMiner

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('wikiMiner', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'wikiMiner.view1',
+  'wikiMiner.view2',
+  'wikiMiner.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
--- app/view2/view2_test.js
+++ app/view2/view2_test.js
@@ -1,8 +1,8 @@
 'use strict';
 
-describe('myApp.view2 module', function() {
+describe('wikiMiner.view2 module', function() {
 
-  beforeEach(module('myApp.view2'));
+  beforeEach(module('wikiMiner.view2'));
 
   describe('view2 controller', function(){
 
