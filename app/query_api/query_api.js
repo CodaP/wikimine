0ad507febbd27c73dfa951dd578490979e318ae9
Coda Phillips <codaphillips@dyn-10-136-34-99.local.uwnet.wisc.edu>
2015-04-18 16:23:05 -0500

    Add query_api

---
+++ app/query_api/query_api.js
@@ -0,0 +1,18 @@
+/**
+ * Created by codaphillips on 4/18/15.
+ */
+(function() {
+    angular.module('wikiMiner.services.query_api', ['ngResource'])
+        .factory('query_api', ['$resource', function($resource){
+            return $resource('http://104.236.226.8/w/api.php', {
+                action:'query',
+                prop:'revisions',
+                format:'json',
+                rvprop:'user|ids|comment',
+                rvlimit:'100',
+                titles:'@titles'
+            },{
+                get:{method:'GET'}
+            });
+        }]);
+})();
