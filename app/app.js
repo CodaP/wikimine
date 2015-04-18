8cbefd7c0c1b31ee788d3b84eb048e07938bcfa5
Coda Phillips <codaphillips@dyn-10-136-34-99.local.uwnet.wisc.edu>
2015-04-18 16:19:13 -0500

    Add query_api service

--- app/app.js
+++ app/app.js
@@ -5,7 +5,8 @@ angular.module('wikiMiner', [
   'ngRoute',
   'wikiMiner.view1',
   'wikiMiner.view2',
-  'wikiMiner.directives.slider'
+  'wikiMiner.directives.slider',
+  'wikiMiner.services.query_api'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
