3407ad64b14f8b2a9d48850b22e69059f6d5e6bf
Martin Wickham <spexguy070@gmail.com>
2015-04-18 22:58:41 -0500

    Load data in map, print to console

--- app/map/map.js
+++ app/map/map.js
@@ -1,4 +1,4 @@
-angular.module('wikiMiner.directives.geoMap', ['uiGmapgoogle-maps'])
+angular.module('wikiMiner.directives.geoMap', ['uiGmapgoogle-maps', 'wikiMiner.services.query_api'])
 
 .directive('geoMap', function () {
     return {
@@ -6,7 +6,7 @@ angular.module('wikiMiner.directives.geoMap', ['uiGmapgoogle-maps'])
         link: function (scope, element, attributes) {
 
         },
-        controller: function($scope, uiGmapGoogleMapApi) {
+        controller: function($scope, uiGmapGoogleMapApi, query_api, geo_api) {
             // Do stuff with your $scope.
             // Note: Some of the directives require at least something to be defined originally!
             // e.g. $scope.markers = []
@@ -24,8 +24,58 @@ angular.module('wikiMiner.directives.geoMap', ['uiGmapgoogle-maps'])
                     styles: [] // TODO: add styles
                 }
             };
+            $scope.preData = [];
+            $scope.addDataPoint = function(data, location) {
+                this.preData.push({data: data, location: location});
+            };
+            $scope.processRevisions = function(data) {
+                // {query-continue: {revisions: {rvcontinue}}, query: {pages: {$id: {pageid, ns, title, revisions: [{revid, parentid, user, anon?, comment}]}}}}
+                var pages = data.query.pages;
+                if (data['query-continue'] != undefined) {
+                    console.log("Continue!");
+                    query_api.get({titles: 'baseball', rvstartid: data['query-continue'].revisions.rvcontinue}, $scope.processRevisions);
+                }
+                for(var id in pages) {
+                    if (pages.hasOwnProperty(id)) {
+                        var page = pages[id];
+                        for (var c = 0; c < page.revisions.length; c++) {
+                            var revData = page.revisions[c];
+                            if (revData.anon !== undefined) {
+                                var ip = $scope.cleanIp(revData.user);
+                                if ($scope.isIp(revData.user)) {
+                                    geo_api.get({query_ip: ip}, function (revData) {
+                                        return function (data) {
+                                            // {ip, country_code, country_name, region_code, region_name, city, zip_code, time_zone, latitude, longitude, metro_code}
+                                            if (data.metro_code != 0) {
+                                                $scope.addDataPoint(revData, {
+                                                    latitude: data.latitude,
+                                                    longitude: data.longitude
+                                                });
+                                            }
+                                        }
+                                    }(revData));
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            $scope.isIp = function(ipStr) {
+                return ipStr.match(/^[0-9A-Fa-f:\\.]+$/);
+            };
+            $scope.cleanIp = function(ipStr) {
+                return ipStr.replace('xxx', '0');
+            };
+            query_api.get({titles: 'baseball'}, $scope.processRevisions);
             uiGmapGoogleMapApi.then(function (maps) {
-                //$scope.map.options.mapTypeId = maps.MapTypeId.TERRAIN;
+                $scope.addDataPoint = function(data, location) {
+                    //TODO: show on the map
+                    console.log('data: '+JSON.stringify(data)+', location: '+JSON.stringify(location));
+                };
+                for (var c = 0; c < $scope.preData.length; c++) {
+                    $scope.addDataPoint(preData[c].data, preData[c].location);
+                }
+                delete $scope.preData;
             });
         },
         scope: {},
