e5484b3622aab76f0b99bcd121bdb9456d97b9ce
Nikhil <nikhiljain15@yahoo.com>
2015-04-19 03:57:26 -0500

    Updated math

--- app/filters/filters.js
+++ app/filters/filters.js
@@ -9,10 +9,17 @@ angular.module('wikiMiner.directives.filters', ['wikiMiner.slider.services'])
             scope.timeBounds = timeBounds;
             scope.scale=scale;
             scope.pageList = [];
-            scope.$watch('timeBounds',function(newValue){
+
+            scope.$watch('timeBounds',function(newValues){
                 scope.maxDate = new Date((scale.maxDate-scale.minDate)*(timeBounds.maxTime/100)+scale.minDate);
                 scope.minDate = new Date((scale.maxDate-scale.minDate)*(timeBounds.minTime/100)+scale.minDate);
+                // timeBounds.maxTime = ((scope.maxDate)/(scale.maxDate))*100;
+
+                 //timeBounds.minTime = ((scope.minDate)/(scale.maxDate))*100;
+                //alert(timeBounds.minTime + " "+ timeBounds.maxTime);
+
             }, true);
+
             scope.addPage = function (pageToAdd) {
                 scope.pageList.push({name: pageToAdd, selected: true});
                 scope.pageToAdd = "";
@@ -21,10 +28,19 @@ angular.module('wikiMiner.directives.filters', ['wikiMiner.slider.services'])
                 scope.pageList.splice(indexToRemove, 1);
             },
             scope.setMinimum = function(){
-
+                scope.minDate=new Date(scale.minDate);
+                scope.updateTime();
+                //timeBounds.minTime = ((scope.minDate)/(scale.maxDate))*100;
             };
             scope.setMaximum = function(){
-                
+                scope.maxDate=new Date(scale.maxDate);
+                scope.updateTime();
+                //timeBounds.maxTime = ((scope.maxDate)/(scale.maxDate))*100;
+            };
+            scope.updateTime = function(){ //updates the timeBounds info, effectively the second watch function
+
+                timeBounds.maxTime = ((scope.maxDate-scale.minDate)/(scale.maxDate))*100;
+                timeBounds.minTime = ((scope.minDate-scale.minDate)/(scale.maxDate))*100;
             }
         },
         scope: {},
