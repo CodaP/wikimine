da88364e2c695f83d6ec949a1d6d58b9daca2074
Coda Phillips <codaphillips@dyn-10-136-34-99.local.uwnet.wisc.edu>
2015-04-18 19:27:27 -0500

    Add time bounds service

--- app/app.js
+++ app/app.js
@@ -3,11 +3,12 @@
 // Declare app level module which depends on views, and components
 angular.module('wikiMiner', [
   'ngRoute',
-  'wikiMiner.directives.slider',
   'wikiMiner.directives.filters',
   'wikiMiner.directives.geoMap',
   'wikiMiner.services.query_api',
   'wikiMiner.services.geo_api',
+  'wikiMiner.controllers',
+  'wikiMiner.slider.services',
   'ui.slider'
 ])
 
