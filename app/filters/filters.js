ba0c2f93601ae832528716e86a9d76e3791df631
Nikhil <nikhiljain15@yahoo.com>
2015-04-18 20:23:39 -0500

    Functionality/form added to filters

--- app/filters/filters.js
+++ app/filters/filters.js
@@ -1,12 +1,28 @@
 angular.module('wikiMiner.directives.filters', [])
 
+
+
 .directive('filters', function () {
     return {
         restrict: 'E',
         link: function (scope, element, attributes) {
 
+            scope.pageList = [];
+            scope.addPage=function(pageToAdd){
+                scope.pageList.push({name: pageToAdd, selected: true});
+                scope.pageToAdd="";
+            };
+            scope.removePage=function(indexToRemove){
+                scope.pageList.splice(indexToRemove,1);
+            }
+            scope.addRegion = function(){
+
+            }
         },
         scope: {},
         templateUrl: 'filters/filters.html'
+
+
+
     }
 });
